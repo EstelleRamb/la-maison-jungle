514a48eb3700889fb8ef2ca005f21ff825c46e5a
estelle rambur <estelle.rambur@gmail.com>
2023-01-30 13:15:28 -0500

    Create componant footer

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,10 @@
+<form onSubmit={handleSubmit}>
+    <input type='text' name='my_input' defaultValue='Tapez votre texte' />
+    <button type='submit'>Entrer</button>
+</form>
+
+
+function handleSubmit(e) {
+  e.preventDefault()
+  alert(e.target['my_input'].value)
+}
